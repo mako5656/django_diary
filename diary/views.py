9ed937790ef9b5bb294924be68d081e9dd1373ac
makoto <makoto09142425@gmail.com>
2020-07-21 22:06:40 +0900

    汎用ビューに書き換え

--- diary/views.py
+++ diary/views.py
@@ -1,67 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
 from .forms import DayCreateForm
 from .models import Day
 
-def index(request):
-    context = {
-        'day_list': Day.objects.all(),
-    }
-    return render(request, 'diary/day_list.html', context)
 
-def add(request):
-    # 送信内容を基にフォームを作る。POSTじゃなければ、空のフォーム
-    form = DayCreateForm(request.POST or None)
+class IndexView(generic.ListView):
+    model = Day
 
-    # method=POST、つまり送信ボタン押下時、入力内容が問題なければ
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('diary:index')
 
-    # 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
-    context = {
-        'form': form
-    }
-    return render(request, 'diary/day_form.html', context)
+class AddView(generic.CreateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:index')
 
-def update(request, pk):
-    # urlのpkを基に、Dayを取得
-    day = get_object_or_404(Day, pk=pk)
 
-    # フォームに、取得したDayを紐付ける
-    form = DayCreateForm(request.POST or None, instance=day)
+class UpdateView(generic.UpdateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:index')
 
-    # method=POST、つまり送信ボタン押下時、入力内容が問題なければ
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('diary:index')
 
-    # 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
-    context = {
-        'form': form
-    }
-    return render(request, 'diary/day_form.html', context)
+class DeleteView(generic.DeleteView):
+    model = Day
+    success_url = reverse_lazy('diary:index')
 
-def delete(request, pk):
-    # urlのpkを基に、Dayを取得
-    day = get_object_or_404(Day, pk=pk)
 
-    # method=POST、つまり送信ボタン押下時、入力内容が問題なければ
-    if request.method == 'POST':
-        day.delete()
-        return redirect('diary:index')
-
-    # 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
-    context = {
-        'day': day,
-    }
-    return render(request, 'diary/day_confirm_delete.html', context)
-
-def detail(request, pk):
-    # urlのpkを基に、Dayを取得
-    day = get_object_or_404(Day, pk=pk)
-
-    # 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
-    context = {
-        'day': day,
-    }
-    return render(request, 'diary/day_detail.html', context)
+class DetailView(generic.DetailView):
+    model = Day
