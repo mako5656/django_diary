0e8dbbe03da558c4385d12d571dc757e4373d8d2
makoto <makoto09142425@gmail.com>
2020-07-21 14:17:04 +0900

    データの更新機能

--- diary/views.py
+++ diary/views.py
@@ -22,3 +22,21 @@ def add(request):
         'form': form
     }
     return render(request, 'diary/day_form.html', context)
+
+def update(request, pk):
+    # urlのpkを基に、Dayを取得
+    day = get_object_or_404(Day, pk=pk)
+
+    # フォームに、取得したDayを紐付ける
+    form = DayCreateForm(request.POST or None, instance=day)
+
+    # method=POST、つまり送信ボタン押下時、入力内容が問題なければ
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+
+    # 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
+    context = {
+        'form': form
+    }
+    return render(request, 'diary/day_form.html', context)
